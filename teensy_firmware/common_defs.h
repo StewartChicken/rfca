aaf0698a06095bd61d8244750a2067715f2bbf33
Evan Poon <109251468+StewartChicken@users.noreply.github.com>
2025-11-17 20:19:19 -0700

    added 50 sample average for A16

--- teensy_firmware/common_defs.h
+++ teensy_firmware/common_defs.h
@@ -2,8 +2,8 @@
 #define _COMMON_DEFS_H
 
 #define LOG_AMP_1               A16
-#define LOG_AMP_READ_DELAY      500 // ms
-#define ADC_REF_VOLTAGE         3.3  // Teensy uses a 3.3V reference for analog inputs
+#define LOG_AMP_READ_DELAY      10 // ms
+#define ADC_REF_VOLTAGE         3.3  // Teensy uses a 3.3 V reference for analog inputs
 #define ADC_MAX_VALUE           1023 // default 10-bit resolution
 
 #endif // _COMMON_DEFS_H
