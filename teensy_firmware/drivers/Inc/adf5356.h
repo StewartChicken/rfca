53082d7803b51aa2960bc0ad57afa83e44aaeb81
Evan Poon <109251468+StewartChicken@users.noreply.github.com>
2026-01-08 11:03:38 -0700

    added citation to adf5356 header

--- teensy_firmware/drivers/Inc/adf5356.h
+++ teensy_firmware/drivers/Inc/adf5356.h
@@ -1,3 +1,6 @@
+// ADF 5355 driver code -- used for reference 
+// https://github.com/analogdevicesinc/no-OS/blob/main/drivers/frequency/adf5355/adf5355.h
+
 #ifndef _ADF5356_H
 #define _ADF5356_H
 
